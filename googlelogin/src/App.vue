abf0162f48907225d1b081cf51256cbd7b124a49
Jongbin <jongbin985@naver.com>
2023-07-07 02:13:00 +0900

    구글로그인 구현

--- googlelogin/src/App.vue
+++ googlelogin/src/App.vue
@@ -1,63 +1,59 @@
 <template>
-  <!-- content에 자신의 OAuth2.0 클라이언트ID를 넣습니다. -->
-<meta name ="google-signin-client_id" content="892284951186-04edfrnds7d8ngftf21v0orcl1a9nqc4.apps.googleusercontent.com">
-  <ul>
-    <li id="GgCustomLogin" @click="loginWithGoogle">
-      <span>Login with Google</span>
-    </li>
-  </ul>
+  <div>
+    <google-signin-button
+      client-id="716095266829-9kjr4qp4r3cacerpvh0ghp8gjujdn2am.apps.googleusercontent.com"
+      @success="onSuccess"
+      @failure="onFailure"
+    ></google-signin-button>
+
+    <div v-if="isSignedIn">
+      <p>Signed in as: {{ user.name }}</p>
+      <button @click="signOut">Sign out</button>
+    </div>
+  </div>
 </template>
 
 <script>
+/* eslint-disable */
+
+import GoogleSignInButton from 'vue-google-signin-button';
+
 export default {
-  name: "GoogleSignIn",
+  components: {
+    GoogleSignInButton,
+  },
+
+  data() {
+    return {
+      isSignedIn: false,
+      user: null,
+    };
+  },
+
+  mounted() {
+    gapi.load('auth2', () => {
+      gapi.auth2.init({
+        client_id: '716095266829-9kjr4qp4r3cacerpvh0ghp8gjujdn2am.apps.googleusercontent.com',
+      });
+    });
+  },
+
   methods: {
-    loginWithGoogle() {
-      gapi.load("auth2", () => {
-        gapi.auth2.init();
-        const options = new gapi.auth2.SigninOptionsBuilder();
-        options.setPrompt("select_account");
-        options.setScope(
-          "email profile openid https://www.googleapis.com/auth/user.birthday.read"
-        );
-        gapi.auth2
-          .getAuthInstance()
-          .signIn(options)
-          .then(
-            this.onSignIn,
-            this.onSignInFailure
-          );
+    onSuccess(user) {
+      this.isSignedIn = true;
+      this.user = user.getBasicProfile();
+    },
+
+    onFailure(error) {
+      console.error(error);
+    },
+
+    signOut() {
+      gapi.auth2.getAuthInstance().signOut().then(() => {
+        this.isSignedIn = false;
+        this.user = null;
       });
     },
-    onSignIn(googleUser) {
-      const access_token = googleUser.getAuthResponse().access_token;
-      this.$http
-        .get("https://people.googleapis.com/v1/people/me", {
-          params: {
-            personFields: "birthdays",
-            key: "AIzaSyAbpzEXkZJDoDh4GnSHxozPzee7M54FHYQ",
-            access_token: access_token
-          }
-        })
-        .then((response) => {
-          console.log(response.data);
-          const profile = googleUser.getBasicProfile();
-          console.log(profile);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    onSignInFailure(t) {
-      console.log(t);
-    }
   },
-  mounted() {
-    const script = document.createElement("script");
-    script.src = "https://apis.google.com/js/platform.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-  }
 };
 </script>
--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id ="wrap">
     <header>
       <a class="logo" href="App.vue">
-        <img src="./assets/mainlogo.png" height="100px">
+        <img src="./assets/mainlogo.png">
       </a>
       <nav>
         <ul class="nav-items">
@@ -29,83 +29,68 @@
 </div>
 <div class="d-grid gap-2 mar">
   
-  <button class="btn btn-outline-secondary text-black" type="button "> Google 계정으로 가입하기</button>
+  <button class="btn btn-outline-secondary text-black" type="button ">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png" style="width: 14px; margin-bottom : 0.5%;"> &nbsp;Google 계정으로 가입하기</button>
   <button class="btn btn-outline-secondary text-black" type="button" style="background-color: #FEE500;"><i class="fa-solid fa-comment"></i>&nbsp;&nbsp;카카오 로그인</button>
   <button class="btn btn-outline-secondary" type="button "><i class="far fa-light fa-envelope evloctaion"></i> 이메일 계정으로 가입하기</button>
 
 </div>
-      </div>  
+</div>  
       
   </div>
 
-
-  <meta name ="google-signin-client_id" content="892284951186-04edfrnds7d8ngftf21v0orcl1a9nqc4.apps.googleusercontent.com">
-  <ul>
-    <li id="GgCustomLogin" @click="loginWithGoogle">
-      <span>Login with Google</span>
-    </li>
-  </ul>
+  <div class="g-signin2" data-onsucess="onSignIn" data-thmem="dark"></div>
+  
 
 </body>
   
-  
-  
+
 
 </template>
 
 <script>
-/* global gapi */
+/* eslint-disable */
 export default {
-  name: "GoogleSignIn",
   methods: {
-    loginWithGoogle() {
-      gapi.load("auth2", () => {
-        gapi.auth2.init();
-        const options = new gapi.auth2.SigninOptionsBuilder();
-        options.setPrompt("select_account");
-        options.setScope(
-          "email profile openid https://www.googleapis.com/auth/user.birthday.read"
-        );
-        gapi.auth2
-          .getAuthInstance()
-          .signIn(options)
-          .then(
-            this.onSignIn,
-            this.onSignInFailure
-          );
-      });
-    },
-    onSignIn(googleUser) {
-      const access_token = googleUser.getAuthResponse().access_token;
-      this.$http
-        .get("https://people.googleapis.com/v1/people/me", {
-          params: {
-            personFields: "birthdays",
-            key: "AIzaSyAbpzEXkZJDoDh4GnSHxozPzee7M54FHYQ",
-            access_token: access_token
-          }
-        })
-        .then((response) => {
-          console.log(response.data);
-          const profile = googleUser.getBasicProfile();
-          console.log(profile);
-        })
-        .catch((error) => {
-          console.log(error);
+    signIn() {
+      gapi.load('auth2', () => {
+        gapi.auth2.init({
+          client_id: 'YOUR_CLIENT_ID'
+        }).then(() => {
+          gapi.auth2.getAuthInstance().signIn().then(googleUser => {
+            var profile = googleUser.getBasicProfile();
+            console.log("ID: " + profile.getId());
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log("Image URL: " + profile.getImageUrl());
+            console.log("Email: " + profile.getEmail());
+
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token);
+          });
         });
-    },
-    onSignInFailure(t) {
-      console.log(t);
+      });
     }
   },
   mounted() {
-    const script = document.createElement("script");
-    script.src = "https://apis.google.com/js/platform.js";
+    // Load the Google Sign-In API script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/platform.js';
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
+
+    script.onload = () => {
+      // gapi object is now available
+      gapi.load('auth2', () => {
+        gapi.auth2.init({
+          client_id: '716095266829-9kjr4qp4r3cacerpvh0ghp8gjujdn2am.apps.googleusercontent.com'
+        });
+      });
+    };
   }
-};
+}
 </script>
 
 <style>
@@ -118,8 +103,8 @@ export default {
   header{
     width: 100%;
     height: 100px;
-    z-index: 2000; /*쌓임 순서*/
-    background-color: #fff;
+    z-index: 100; /*쌓임 순서*/
+    background-color: #fff; 
     box-shadow: 0 5px 2px rgba(0, 0, 0, 0.05), 0 1px 0 rgba(0, 0, 0, 0.05);
   }
   
@@ -178,6 +163,7 @@ export default {
 .location{
   position: relative;
   top: 150px;
+  margin-top: 30px;
 }
 
 .color>input{
@@ -196,6 +182,9 @@ export default {
 .mar{
   margin-top:70px;
 }
-
-
+.hello{
+  background-color: black;
+  width: 100px;
+  height: 100px;
+}
 </style>
